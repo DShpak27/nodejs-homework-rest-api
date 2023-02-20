4e1853f0eeed4033271b1a3fc8707e90daa95e38
Денис Шпак <mr.shpak@gmail.com>
2023-02-20 22:40:10 +0200

    made_models

--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,77 @@
-// const fs = require('fs/promises')
+const { nanoid } = require("nanoid");
+const fs = require("fs/promises");
+const path = require("path");
 
-const listContacts = async () => {}
+const generateUniqueId = () => nanoid(5);
 
-const getContactById = async (contactId) => {}
+const contactsFilePath = path.join(__dirname, "models", "contacts.json");
 
-const removeContact = async (contactId) => {}
+async function listContacts() {
+    try {
+        const unparsedContacts = fs.readFile(contactsFilePath, "utf8");
+        return JSON.parse(unparsedContacts);
+    } catch (error) {
+        return error.message;
+    }
+}
 
-const addContact = async (body) => {}
+async function getContactById(contactId) {
+    try {
+        const unparsedContacts = fs.readFile(contactsFilePath, "utf8");
+        const contacts = JSON.parse(unparsedContacts);
+        const contactById = contacts.find(({ id }) => id === contactId);
+        return contactById;
+    } catch (error) {
+        return error.message;
+    }
+}
 
-const updateContact = async (contactId, body) => {}
+async function removeContact(contactId) {
+    try {
+        const unparsedContacts = fs.readFile(contactsFilePath, "utf8");
+        const contacts = JSON.parse(unparsedContacts);
+        const updatedContacts = contacts.filter(({ id }) => id !== contactId);
+        const unparsedUpdatedContacts = JSON.stringify(updatedContacts);
+        await fs.writeFile(contactsFilePath, unparsedUpdatedContacts);
+    } catch (error) {
+        return error.message;
+    }
+}
+
+async function addContact(body) {
+    const newContact = { ...body, id: generateUniqueId() };
+    try {
+        const unparsedContacts = fs.readFile(contactsFilePath, "utf8");
+        const contacts = JSON.parse(unparsedContacts);
+        const updatedContacts = [newContact, ...contacts];
+        const unparsedUpdatedContacts = JSON.stringify(updatedContacts);
+        await fs.writeFile(contactsFilePath, unparsedUpdatedContacts);
+        return newContact;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+async function updateContact(contactId, body) {
+    try {
+        const unparsedContacts = fs.readFile(contactsFilePath, "utf8");
+        const contacts = JSON.parse(unparsedContacts);
+        const contactToUpdate = contacts.find(({ id }) => id === contactId);
+        const updatedContact = { ...contactToUpdate, ...body };
+        const contactsToUpdate = contacts.filter(({ id }) => id !== contactId);
+        const updatedContacts = [...contactsToUpdate, updatedContact];
+        const unparsedUpdatedContacts = JSON.stringify(updatedContacts);
+        await fs.writeFile(contactsFilePath, unparsedUpdatedContacts);
+        return updatedContact;
+    } catch (error) {
+        return error.message;
+    }
+}
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-}
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact,
+    updateContact,
+};
